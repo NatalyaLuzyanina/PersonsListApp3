71700fed65ade821bb199c5c795eeca21254afde
NatalyaLuzyanina <lnp3395@gmail.com>
2021-05-13 22:41:39 +0300

    it works

--- PersonsListApp/NetworkManager.swift
+++ PersonsListApp/NetworkManager.swift
@@ -17,29 +17,31 @@ class NetworkManager {
     func fetchPersons(url: String, completion: @escaping ([Person]) -> Void ) {
         
         AF.request(URLExamples.dataURL.rawValue)
+            // проверка валидности данных
             .validate()
             .responseJSON { dataResponse in
                 
                 switch dataResponse.result {
                 case .success(let value):
-
-                    guard let personsData = value as? [String : AnyObject] else { return }
                     
-                    guard let persons = personsData["data"] else { return }
-          
-                    var personsArray: [Person] = []
-                    for person in Array(arrayLiteral: persons) {
+                    guard let value = value as? [String : Any] else { return }
+                    guard let personsData = value["data"] as? [[String : Any]] else { return }
+//                    guard let persons = personsData["data"] else { return }
+//
+                    var persons: [Person] = []
+                   for person in personsData {
                         let person = Person(id: person["id"] as? Int,
                                             email: person["email"] as? String,
                                             first_name: person["first_name"] as? String,
                                             last_name: person["last_name"] as? String,
                                             avatar: person["avatar"] as? String)
-                    
-                        personsArray.append(person)
-                    }
+                    print(person)
+
+                        persons.append(person)
+                   }
                  
                     DispatchQueue.main.async {
-                        completion(personsArray)
+                        completion(persons)
 
                     }
                 case .failure(let error):
